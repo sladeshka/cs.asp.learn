ac9d8c1c4cbe7b1d50e2fa134e6a16f9fc68d7c5
sladeshka <130031mail@mail.ru>
2024-04-10 00:02:15 +0300

    lab 5

---
+++ lab_mvc_5/MvcCreditApp/Models/Bid.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcCreditApp.Models
+{
+    public class Bid
+    {
+        public virtual int BidId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string CreditHead { get; set; }
+        public virtual DateTime bidDate { get; set; }
+    }
+}
---
+++ lab_mvc_5/MvcCreditApp/Models/Credit.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcCreditApp.Models
+{
+    public class Credit
+    {
+        public virtual int CreditId { get; set; }
+        public virtual string Head { get; set; }
+        public virtual int Period { get; set; }
+        public virtual int Sum { get; set; }
+        public virtual int Procent { get; set; }
+    }
+}
